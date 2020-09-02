f3181062a208c3bc036fa84e48b2cc0cc40f4c6d
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-09-02 23:58:09 +0800

    commit by ng

--- app/src/main/java/com/example/test1/HomeFragment.java
+++ app/src/main/java/com/example/test1/HomeFragment.java
@@ -20,64 +20,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
-    private Handler sliderHandler = new Handler();
-    private ViewPager2 viewPager2;
-   
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewPager2= viewPager2.findViewById(R.id.viewPagerImageSlider);
-        List<SliderItem> sliderItems=new ArrayList<>();
-        sliderItems.add(new SliderItem(R.drawable.digimon_adventure___last_evolution_kizuna));
-        sliderItems.add(new SliderItem(R.drawable.my_hero_academia___heros_rising));
-        sliderItems.add(new SliderItem(R.drawable.hitman___agent_jun));
-        sliderItems.add(new SliderItem(R.drawable.baba_yaga___terror_of_the_dark_forest));
-        sliderItems.add(new SliderItem(R.drawable.jumanji2));
-        viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
-        viewPager2.setClipToPadding(false);
-        viewPager2.setClipChildren(false);
-        viewPager2.setOffscreenPageLimit(3);
-        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-
-        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
-        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull View page, float position) {
-                float r=1-Math.abs(position);
-                page.setScaleY(0.85f + r * 0.15f);
-            }
-        });
-
-        viewPager2.setPageTransformer(compositePageTransformer);
-
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                sliderHandler.removeCallbacks(sliderRunnable);
-                sliderHandler.postDelayed(sliderRunnable,3000);
-            }
-        });
 
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
-    private Runnable sliderRunnable = new Runnable() {
-        @Override
-        public void run() {
-            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
-        }
-    };
 
-    @Override
-    public void onPause(){
-        super.onPause();
-        sliderHandler.removeCallbacks(sliderRunnable);
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        sliderHandler.postDelayed(sliderRunnable,3000);
-    }
 }
--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -28,6 +28,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     LinearLayout Popular,Coming_soon,Now_showing;
+    private Handler sliderHandler = new Handler();
+    private ViewPager2 viewPager2;
 
     private DrawerLayout drawer;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -58,6 +60,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Popular = (LinearLayout)findViewById(R.id.Popular2);
         Coming_soon = (LinearLayout)findViewById(R.id.Now_showing2);
         Now_showing= (LinearLayout)findViewById(R.id.Coming_soon2);
+
+        viewPager2= viewPager2.findViewById(R.id.viewPagerImageSlider);
+        List<SliderItem> sliderItems=new ArrayList<>();
+        sliderItems.add(new SliderItem(R.drawable.digimon_adventure___last_evolution_kizuna));
+        sliderItems.add(new SliderItem(R.drawable.my_hero_academia___heros_rising));
+        sliderItems.add(new SliderItem(R.drawable.hitman___agent_jun));
+        sliderItems.add(new SliderItem(R.drawable.baba_yaga___terror_of_the_dark_forest));
+        sliderItems.add(new SliderItem(R.drawable.jumanji2));
+        viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r=1-Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable,3000);
+            }
+        });
+
 
     }
 
@@ -128,6 +165,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return false;
+    }
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    };
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable,3000);
     }
 
 
